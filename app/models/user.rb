e6707737fc65b75ca303891ea40de9d8546cb9b3
Gabriel Paiva <gabrieldcpaiva@gmail.com>
2020-06-09 15:59:48 -0300

    Created interactions and a simple seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :videos, dependent: :destroy
+  has_many :interactions, dependent: :destroy
 end
--- db/migrate/20200609182134_create_interactions.rb
+++ db/migrate/20200609182134_create_interactions.rb
@@ -1,11 +1,11 @@
 class CreateInteractions < ActiveRecord::Migration[6.0]
   def change
     create_table :interactions do |t|
-      t.boolean :viewed_on
+      t.datetime :viewed_on
       t.references :user, null: false, foreign_key: true
       t.references :video, null: false, foreign_key: true
-      t.boolean :favourite
-      t.boolean :viewed
+      t.boolean :favourite, null: false, default: false
+      t.boolean :viewed, null: false, default: false
       t.integer :difficulty
       t.integer :rating
 

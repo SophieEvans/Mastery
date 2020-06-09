c74a6a6973917e46b8c90d939dca552357fff1d3
Gabriel Paiva <gabrieldcpaiva@gmail.com>
2020-06-09 19:42:44 -0300

    Created the video validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :user
   belongs_to :sub_category
+
+  validates :drill, presence: { unless: :tutorial? }
+  validates :tutorial, presence: { unless: :drill? }
 end

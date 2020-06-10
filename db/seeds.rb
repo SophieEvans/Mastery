5b6fae333ce6781bec030438b1f4a6435487e94f
Gabriel Paiva <gabrieldcpaiva@gmail.com>
2020-06-10 15:52:24 -0300

    Changed video difficulties to a String

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,8 @@ traditional = SubCategory.create!(name: "Traditional", category_id: pushups.id)
 staggered = SubCategory.create!(name: "Staggered"), category_id: pushups.id
 superman = SubCategory.create!(name: "Superman", category_id: pushups.id)
 
-video = Video.create!(drill: true, tutorial: true, rating: 0, difficulty: 1,
-  user_id: gabriel.id, sub_category_id: backflip.id, you_tube_key: "uC5tlDPxwAU")
+video = Video.create!(drill: true, tutorial: true, rating: 0, difficulty: "Beginner",
+  user_id: gabriel.id, sub_category_id: backflip.id, you_tube_key: “uC5tlDPxwAU”)
 
 interaction1 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video.id, viewed: true)

b60970d0cda10024d92128bd34d601f7bc02cfae
Gabriel Paiva <gabrieldcpaiva@gmail.com>
2020-06-11 17:57:00 -0300

    Changed video models and others that needed fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,17 +16,17 @@ hollowback = SubCategory.create!(name: "Hollowback", category_id: handstands.id)
 straight = SubCategory.create!(name: "Straight", category_id: handstands.id)
 garudasana = SubCategory.create!(name: "Garudasana", category_id: handstands.id)
 # videos and interactions
-video_one_armed = Video.create!(drill: true, tutorial: false, rating: 0, difficulty: "Advanced",
+video_one_armed = Video.create!(title: "ONE ARM HANDSTAND 3 BEST EXERCISES", rating: 0, difficulty: "Advanced",
   user_id: gabriel.id, sub_category_id: one_armed.id, you_tube_key: "f7bOSY83548")
 interaction = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_one_armed.id, viewed: true)
 
-video_one_armed1 = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Advanced",
+video_one_armed1 = Video.create!(title: "Yoga one-handed handstand (at the wall)", rating: 0, difficulty: "Advanced",
   user_id: gabriel.id, sub_category_id: one_armed.id, you_tube_key: "eXRrcSIE7G4")
 interaction1 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_one_armed1.id, viewed: true)
 
-video_one_armed2 = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Advanced",
+video_one_armed2 = Video.create!(title: "How to Do a One-Handed Handstand", rating: 0, difficulty: "Advanced",
   user_id: gabriel.id, sub_category_id: one_armed.id, you_tube_key: "CYWIk-mjbiM")
 interaction2 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_one_armed.id, viewed: true)
@@ -39,17 +39,17 @@ backflip = SubCategory.create!(name: "Backflip", category_id: flips.id)
 aerial = SubCategory.create!(name: "Aerial", category_id: flips.id)
 backside = SubCategory.create!(name: "Backside", category_id: flips.id)
 # videos and interactions
-video_backflip = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Intermediate",
+video_backflip = Video.create!(title: "Tutorial of how to do a backflip", rating: 0, difficulty: "Intermediate",
   user_id: gabriel.id, sub_category_id: backflip.id, you_tube_key: "VIcdyNfwe5Q")
 interaction3 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_backflip.id, viewed: true)
 
-video_backflip1 = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Intermediate",
+video_backflip1 = Video.create!(title: "Backflip on the ground tutorial (explained)", rating: 0, difficulty: "Intermediate",
   user_id: gabriel.id, sub_category_id: backflip.id, you_tube_key: "gBm1nAM_tww")
 interaction4 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_backflip1.id, viewed: true)
 
-video_backflip2 = Video.create!(drill: true, tutorial: false, rating: 0, difficulty: "Intermediate",
+video_backflip2 = Video.create!(title: "Tuck Conditioning Drills", rating: 0, difficulty: "Intermediate",
   user_id: gabriel.id, sub_category_id: backflip.id, you_tube_key: "T_DYae728C4")
 interaction5 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_backflip2.id, viewed: true)
@@ -62,17 +62,17 @@ traditional = SubCategory.create!(name: "Traditional", category_id: pushups.id)
 staggered = SubCategory.create!(name: "Staggered", category_id: pushups.id)
 superman = SubCategory.create!(name: "Superman", category_id: pushups.id)
 # videos and interactions
-video_wide_grip_pushup = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Intermediate",
+video_wide_grip_pushup = Video.create!(title: "How To Do A WIDE GRIP PUSH UP | Exercise Demonstration Video and Guide", rating: 0, difficulty: "Intermediate",
   user_id: gabriel.id, sub_category_id: wide_grip_pushup.id, you_tube_key: "Z8nUVpSZAhE")
 interaction6 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_wide_grip_pushup.id, viewed: true)
 
-video_wide_grip_pushup1 = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Beginner",
+video_wide_grip_pushup1 = Video.create!(title: "Perfect Push Up Workout - Push Up (Wide)", rating: 0, difficulty: "Beginner",
   user_id: gabriel.id, sub_category_id: wide_grip_pushup.id, you_tube_key: "NJPS2d5JME0")
 interaction7 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_wide_grip_pushup1.id, viewed: true)
 
-video_wide_grip_pushup2 = Video.create!(drill: false, tutorial: true, rating: 0, difficulty: "Intermediate",
+video_wide_grip_pushup2 = Video.create!(title: "Best Push Up Workout: Chest & Abs", rating: 0, difficulty: "Intermediate",
   user_id: gabriel.id, sub_category_id: wide_grip_pushup.id, you_tube_key: "d4lT37mRRPM")
 interaction8 = Interaction.create!(viewed_on: DateTime.now, user_id: sophie.id,
   video_id: video_wide_grip_pushup2.id, viewed: true)

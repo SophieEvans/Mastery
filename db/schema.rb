bfc297faf6b23f21fd9f1b95ea49e974beca1885
Sophie Evans <emailsophiep@gmail.com>
2020-06-17 23:37:55 -0300

    carousel CSS

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 2020_06_17_180637) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed"
+    t.boolean "helpful", default: false
+    t.boolean "good_style", default: false
+    t.boolean "vote"
     t.index ["user_id"], name: "index_interactions_on_user_id"
     t.index ["video_id"], name: "index_interactions_on_video_id"
   end
@@ -78,7 +81,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_180637) do
   create_table "videos", force: :cascade do |t|
     t.integer "helpful"
     t.integer "good_style"
-    t.integer "rating", default: 0
+    t.integer "rating"
     t.bigint "user_id", null: false
     t.bigint "sub_category_id", null: false
     t.string "you_tube_key"
